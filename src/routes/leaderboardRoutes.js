8edb9bf908aa3f94e805fb5c79754cb3a31b111a
Enache Alex <alexenache43@yahoo.com>
2022-04-01 19:39:03 +0300

    sort leaderboard

--- src/routes/leaderboardRoutes.js
+++ src/routes/leaderboardRoutes.js
@@ -8,16 +8,19 @@ export default (app) => {
     app.use(cors());
 
     app.get(`${serverConfig.BASE_URL}/getLeaderboard/:region/:gameMode`, cors(), async (req, res) => {
-        const response = await fetch(`${serverConfig.PUBG_API}/${req.params.region}/leaderboards/${serverConfig.SEASON}/${req.params.gameMode}`, 
-        {
-            headers: {
-                'Authorization': `Bearer ${serverConfig.API_KEY}`, 
-                'Accept': 'application/vnd.api+json'
-            }
-        }, cors());
-        
+        const response = await fetch(`${serverConfig.PUBG_API}/${req.params.region}/leaderboards/${serverConfig.SEASON}/${req.params.gameMode}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${serverConfig.API_KEY}`,
+                    'Accept': 'application/vnd.api+json'
+                }
+            }, cors());
+
         const data = await response.json();
 
-        res.status(200).json(data);
+        const leaderboardTop100 = data.included.filter(item => item.attributes.rank <= 100);
+        const sortedLeaderboardTop100 = leaderboardTop100.sort((a, b) => a.attributes.rank - b.attributes.rank);
+
+        res.status(200).json(sortedLeaderboardTop100);
     });
 }
