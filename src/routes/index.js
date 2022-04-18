6f24f318be100a8b9cbc965c5390bf0084a21b5a
Enache Alex <alexenache43@yahoo.com>
2022-04-18 17:17:02 +0300

    Add-api-for-survival-and-weapon-masteries

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import leaderboardRoutes from './leaderboardRoutes.js';
+import playerRoutes from './playerRoutes.js';
 
 export default (app) => {
     leaderboardRoutes(app);
+    playerRoutes(app);
 }
---
+++ src/routes/playerRoutes.js
@@ -0,0 +1,37 @@
+import fetch from "node-fetch";
+import cors from 'cors';
+
+import serverConfig from '../config/index.js';
+
+export default (app) => {
+
+    app.use(cors());
+
+    app.get(`${serverConfig.BASE_URL}/getSurvivalMastery/:region/:accountId`, cors(), async (req, res) => {
+        const response = await fetch(`${serverConfig.PUBG_API}/${req.params.region}/players/${req.params.accountId}/survival_mastery`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${serverConfig.API_KEY}`,
+                    'Accept': 'application/vnd.api+json'
+                }
+            }, cors());
+
+        const data = await response.json();
+
+        res.status(200).json(data);
+    });
+
+    app.get(`${serverConfig.BASE_URL}/getWeaponMastery/:region/:accountId`, cors(), async (req, res) => {
+        const response = await fetch(`${serverConfig.PUBG_API}/${req.params.region}/players/${req.params.accountId}/weapon_mastery`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${serverConfig.API_KEY}`,
+                    'Accept': 'application/vnd.api+json'
+                }
+            }, cors());
+
+        const data = await response.json();
+
+        res.status(200).json(data);
+    });
+}
