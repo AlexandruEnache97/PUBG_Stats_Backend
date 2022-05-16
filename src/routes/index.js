dca8aa1163b8cd2adabf3fd7d5d6a52de1fbaeb0
Enache Alex <alexenache43@yahoo.com>
2022-05-16 16:38:25 +0300

    Set endpoint for save tested weapons

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import leaderboardRoutes from './leaderboardRoutes.js';
 import playerRoutes from './playerRoutes.js';
+import weaponsRoutes from './weaponsRoutes.js';
 
 export default (app) => {
     leaderboardRoutes(app);
     playerRoutes(app);
+    weaponsRoutes(app);
 }
